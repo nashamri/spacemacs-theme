3a0f1c62c134e24e84fa6f8e3a2788f90dfaab38
nashamri <designernasser@gmail.com>
2016-07-07 14:44:41 +0300

    Reverting last PR (#53)

--- spacemacs-common.el
+++ spacemacs-common.el
@@ -159,7 +159,7 @@
      `(font-lock-type-face ((,class (:foreground ,type :inherit bold))))
      `(font-lock-variable-name-face ((,class (:foreground ,var))))
      `(font-lock-warning-face ((,class (:foreground ,war :background ,bg1))))
-     `(fringe ((,class (:background ,bg2 :foreground ,base))))
+     `(fringe ((,class (:background ,bg1 :foreground ,base))))
      `(highlight ((,class (:foreground ,base :background ,highlight))))
      `(hl-line ((,class (:background ,bg2))))
      `(isearch ((,class (:foreground ,bg1 :background ,mat))))

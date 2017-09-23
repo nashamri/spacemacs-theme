8cf00e74aea15c592baf7107cc560397e5d5769e
Mathew Robinson <chasinglogic@gmail.com>
2017-09-23 01:13:40 -0400

    Add support for Emacs 26 display-line-numbers-mode

--- spacemacs-common.el
+++ spacemacs-common.el
@@ -515,6 +515,10 @@ to 'auto, tags may not be properly aligned. "
 ;;;;; linum-mode
      `(linum ((,class (:foreground ,lnum :background ,bg2))))
 
+;;;;; line-numbers-mode (Emacs 26+)
+     (when (>= emacs-major-version 26)
+       `(line-number ((,class (:foreground ,lnum :background ,bg2)))))
+
 ;;;;; linum-relative
      `(linum-relative-current-face ((,class (:foreground ,comp))))
 

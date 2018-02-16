00356dc91c7301bb7f8fcd1a2a9070055784d141
Vincent Zhang <seagle0128@gmail.com>
2018-02-16 18:13:16 +0800

    Fix mismatch parens.

--- spacemacs-common.el
+++ spacemacs-common.el
@@ -532,8 +532,9 @@ to 'auto, tags may not be properly aligned. "
 
 ;;;;; display-line-numbers-mode (Emacs 26+)
      (when (>= emacs-major-version 26)
-       `(line-number ((,class (:foreground ,lnum :background ,bg2)))))
-       `(line-number-current-line ((,class (:foreground ,base :background ,bg2))))))
+       `(line-number ((,class (:foreground ,lnum :background ,bg2))))
+       `(line-number-current-line ((,class (:foreground ,base :background ,bg2)))))
+
 ;;;;; linum-relative
      `(linum-relative-current-face ((,class (:foreground ,comp))))
 
